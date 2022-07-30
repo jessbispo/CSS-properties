fbdada2e02bdcbac311528e537fa0387dd524d40
jessica <jessicabispo.work@gmail.com>
2022-07-30 12:10:09 -0300

    feat: finalizando projeto

--- css-tricks.js
+++ css-tricks.js
@@ -3,10 +3,7 @@ import readLineSync from 'readline-sync';
 
 //readline-sync pacote, pra usar: var varName = readlineSync.question('May I have your name? ');
 
-//ainda não esta ordenado por letras maiusculas e minusculas
-
 const arr = []
-
 
 while(true) {
 const questUser = readLineSync.question('Digite uma propriedade CSS e retornaremos uma lista ordenada pra voce: ')
@@ -16,6 +13,6 @@ const questUser = readLineSync.question('Digite uma propriedade CSS e retornarem
         arr.push(questUser)
     }
 }
-const outPut = arr.sort() //ordena de a-z, entretanto, ordena primeiro caracteres maiusculos e depois os minusculos
-console.log(outPut.join("\n"))
+const outP = arr.sort() //ordena de a-z, entretanto, ordena primeiro caracteres maiusculos e depois os minusculos
+console.log(outP.join(" "))
     
